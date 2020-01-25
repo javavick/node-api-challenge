ce6ec8c043ce663b86866253b8e00bd228968142
Vick Javariz <51038821+javavick@users.noreply.github.com>
2020-01-24 22:45:01 -0400

    Add GET for '/projects/:id'

--- projects/projectsRouter.js
+++ projects/projectsRouter.js
@@ -8,7 +8,26 @@ router.get("/", (req, res) => {
   Projects.get()
     .then((projects) => res.status(200).json(projects))
     .catch(() =>
-      res.status(500).json({ error: "The data could not be retrieved." })
+      res.status(500).json({ error: "The projects could not be retrieved." })
+    );
+});
+
+// GET ("/projects/:id")
+router.get("/:id", (req, res) => {
+  Projects.get(req.params.id)
+    .then((project) => {
+      if (!project) {
+        res
+          .status(404)
+          .json({ message: "The specified project does not exist." });
+      } else {
+        res.status(200).json(project);
+      }
+    })
+    .catch(() =>
+      res
+        .status(500)
+        .json({ error: "The specified project could not be retrieved." })
     );
 });
 
